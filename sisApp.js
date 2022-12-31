5640e0d6ceee8d828ec740071adb264ded0bf6a4
Yerko Cortés Baeza <yerkodigo@gmail.com>
2022-12-31 20:33:18 -0300

    Update project

--- sisApp.js
+++ sisApp.js
@@ -1,29 +1,39 @@
 let app = angular.module('sismoApp', []);
-
 app
   .controller('SismoAppController', function($scope, $http) {
     $scope.dataCurrentSismos = [];
 
-    $http({
-      method: 'GET',
-      url: 'https://api.gael.cloud/general/public/sismos'
-    })
-      .then(function successCallback(response) {
+    $scope.init = function() {
+      $scope.dataCurrentSismos = $scope.getUltimosSismos();
+    }
 
-        response.data.slice(0, 3).forEach((e) => {
-          let date = new Date(e.Fecha);
+    $scope.getUltimosSismos = function() {
+      $http({
+        method: 'GET',
+        url: 'https://api.gael.cloud/general/public/sismos'
+      })
+        .then(function successCallback(response) {
 
-          $scope.dataCurrentSismos.push({
-            fecha: date.toLocaleDateString('es-es', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' }),
-            hora: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} hrs.`,
-            fechaUpdate: e.FechaUpdate,
-            magnitud: e.Magnitud,
-            profundidad: e.Profundidad,
-            refGeografica: e.RefGeografica
-          })
-        })
+          if(response.status == 200) {
+            $scope.dataCurrentSismos = response.data.map(e => {
+              let date = new Date(e.Fecha);
+              let dateFormated = date.toLocaleDateString('es-es', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' });
 
-      }, function errorCallback(error) {
-        console.log(error);
-      });
+              let actualizacion = new Date(e.FechaUpdate);
+              let actualizacionFormated = actualizacion.toLocaleDateString('es-es', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' });
+              return {
+                fecha: dateFormated.charAt(0).toUpperCase() + dateFormated.slice(1),
+                hora: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} hrs.`,
+                fechaUpdate: `${actualizacionFormated} - ${actualizacion.getHours()}:${actualizacion.getMinutes()}:${actualizacion.getSeconds()} hrs.`,
+                magnitud: e.Magnitud,
+                profundidad: e.Profundidad + " km",
+                refGeografica: e.RefGeografica
+              }
+            })
+          }
+        }, function errorCallback(error) {
+          console.log(error);
+        });
+    }
+
   });
